c6831d106849a3b7de2a656fdeb7bc7e53e5c2aa
HotJuly <949588380@qq.com>
2023-03-31 16:40:49 +0800

    16.浏览器多线程

--- 02.代码/01.js基础/07.worker.js
+++ 02.代码/01.js基础/07.worker.js
@@ -1,6 +1,9 @@
 onmessage = function(e){
     // console.log(e)
     // eval("console.log(22222222)")
+
+    // eval函数可以将一段字符串作为js代码执行,内部会生成一个独立的作用域
+    // 也就是说外接无法读取里面的变量
     eval(e.data);
     // postMessage(777)
 }
