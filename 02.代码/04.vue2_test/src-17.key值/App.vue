ac757b1fd009bab54994961bae588f32eed4143a
HotJuly <949588380@qq.com>
2023-04-11 10:23:33 +0800

    71.单页面应用了解

--- 02.代码/04.vue2_test/src-17.key值/App.vue
+++ 02.代码/04.vue2_test/src-17.key值/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ul>
+    <li v-for="(item,index) in arr" :key="index">
+      <label>{{item}} : </label>
+      <input type="text">
+    </li>
+  </ul>
+  <button @click="handler">+6</button>
   </div>
 </template>
 
@@ -9,6 +15,16 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data(){
+    return{
+      arr:[1,2,3,4,5]
+    }
+  },
+  methods:{
+    handler(){
+      this.arr.splice(1,0,6)
+    }
+  },
   components: {
     HelloWorld
   }
